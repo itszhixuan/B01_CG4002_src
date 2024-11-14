c7656249ef50402d53912f76f3a9e493e21ee06f
itszhixuan <leezhixuan001@gmail.com>
2024-11-14 21:56:44 +0800

    Consolidation of files used for whole semester

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # capstoneAI
+
+Houses the code used for entire AI process from HW to SW, across 13 weeks of CG4002 capstone project. Project aims to predict certain moves based on list of actions, where the user will execute the moves in real time. IMUs are strapped on the player's right hand and leg, and data is sent to the Ultra96 FPGA through internal and external communications(not covered in this repo). When the data is received by the python script with PYNQ overlay, an output is predicted by the IP block generated from vivado (in the form of bitstream) and returned to the external comms, for further dissemination to the visualiser and back to internal communications. A temperature scaling confidence level scheme was attempted to filter out noise and unsure predictions based on the data sent from the player. Everything runs on the Programming Logic (PL) on the Ultra96, and communication is done using AXI DMA.
+
+## List of moves
+### Values corresponding to each action 
+- Bowling = 1 
+- Logout = 2 
+- Rainbomb = 3 
+- Reload = 4 
+- Shield = 5 
+- Soccer = 6 
+- Volleyball = 7 
+- Walk = 8 
+- Continuous Walk = 9
+
+Additionally, all predictions made that fall below the confidence threshold of 0.94 are reclassified to [9], as it is not an action of interest to other stakeholders in this system.
+
+## Initial training of MLP model
+The model is trained on a simple MLP model that is used to predict the moves based on waveforms collected. Initially, it was trained by collecting various features such as min, max, std etc but was found to be less consistent between players (due to different degrees and magnitudes of movement). The raw data is subsequently used to analyse the trends of each move when plotted against time, therefore giving a more generic prediction that suits players of various heights and arm length. `mlp_python` and `old_mlp_python` houses the abovementioned changes to the MLP model.
+
+## Collection of data
+With the help of internal comms, training data is collected from our IMUs and this will be used to train our MLP model. Data can be found in `collected_data`. Prior to our hardware being ready for data collection, an online dataset is used to help us train and set up the entire pipeline needed for our HW and SW AI. This can be found in `onlineset`, which was taken from the following author on [Kaggle](https://www.kaggle.com/datasets/harrisonlou/imu-glove/data).
+
+## Converting of MLP model to C++
+To create a bitstream in Vivado, we can utilise the High-level synthesis function that is provided by Vitis to convert C++ into an IP block. This can help us avoid using Verilog to rewrite the MLP model that we want. The MLP model is then rewritten into C++ with the weights trained from the Python side of the model, which allows us to recreate the exact same model that we want on our bitstream. Utilising `hls::stream` helps us recieve and send data from our Python script that we will be creating on the Ultra96. This can be found in `hls_cplusplus`.
+
+## Overlaying the bitstream and using PYNQ
+When the bitstream is complete, it is transfer (along with the hwh file) to the FPGA and the overlay is done using PYNQ. The data can then be sent and recieved, with the bitstream acting as the AI which serves as our prediction black box. The code can be found in `old_fpga_python` and `fpga_python`, with the main difference being how the AI component communicates with the external comms. 
+
---
+++ fpga_python/confidence.py
@@ -0,0 +1,224 @@
+# server.py
+import socket
+import threading
+from threading import Thread
+import time
+from pynq import Overlay, allocate
+import pynq.lib.dma
+import sys, os
+import numpy as np
+import struct
+import sys
+import ast
+import time
+import signal
+
+N_SAMPLES = 80
+N_FEATURES = 6
+N_WINDOWS = 38
+
+overlay = None
+dma_send = None
+dma_recv = None
+input_buffer = None
+output_buffer = None
+
+def initialise():
+    
+    global overlay, dma_send, dma_recv, input_buffer, output_buffer
+    
+    curr_dir = sys.path[0]
+    overlay = Overlay(os.path.join(curr_dir, 'confidence_logits.bit'))
+
+    dma = overlay.dma
+    dma_send = dma.sendchannel
+    dma_recv = dma.recvchannel
+
+    input_buffer = allocate(shape=(456,), dtype=np.int32)
+    output_buffer = allocate(shape=(1,), dtype=np.int32)
+    
+    print("Overlay setup done")
+
+def preprocess(raw_data):
+    # [[[12776, -3608, -12876, -261, -57, 669], ... (80x6x2) 
+    def smoothen_imu_data(data, window_size=5, stride=2):
+        # [[12776, -3608, -12876, -261, -57, 669], ... (80x6)
+
+        data = np.array(data)
+        shape = (N_WINDOWS, window_size, N_FEATURES)
+        strides = (data.strides[0] * stride, *data.strides)
+        sliding_windows = np.lib.stride_tricks.as_strided(data, shape=shape, strides=strides)
+
+        smoothened_imu = sliding_windows.mean(axis=1)
+        # Output: (38x6)
+        return smoothened_imu
+
+    hand_df = smoothen_imu_data(raw_data[0])
+    leg_df = smoothen_imu_data(raw_data[1])
+
+    flattened = np.concatenate((hand_df.flatten(), leg_df.flatten()))
+    return flattened.tolist()
+
+def float_to_int_array(floats):
+    return np.frombuffer(np.array(floats, dtype=np.float32).tobytes(), dtype=np.int32)
+
+def temperature_scaled_probability(logits, temperature):
+    # Convert logits to numpy array if they're not already
+    logits = np.array(logits)
+    
+    # Scale the logits by dividing by the temperature
+    scaled_logits = logits / temperature
+    
+    # Apply softmax to get the probabilities
+    exp_logits = np.exp(scaled_logits - np.max(scaled_logits))  # Subtract max for numerical stability
+    probabilities = exp_logits / exp_logits.sum()
+    
+    # Get the probability of the predicted (highest logit) value
+    predicted_index = np.argmax(logits)  # Find the index of the max logit in original logits
+    predicted_probability = probabilities[predicted_index]
+    
+    return predicted_probability
+
+def generate_output(output_array):
+    return output_array.index(max(output_array))
+
+def MLmodel(raw_data):
+    start_time = time.time()
+    print(raw_data)
+    processed_data = preprocess(raw_data)
+    end_time = time.time()
+    print('sliding window time taken:', end_time - start_time)
+    print(processed_data)
+    start_time = time.time()
+    int_data = float_to_int_array(processed_data)
+    end_time = time.time()
+    print('float to int time taken:', end_time - start_time)
+    print(int_data)
+    
+#     print(int_data)
+    input_buffer[:] = int_data
+
+#     output_size = len(output_buffer)
+#     output_buffer.resize(output_size * 2)
+    
+    output_buffer.fill(0)
+    dma_send.transfer(input_buffer)
+    dma_recv.transfer(output_buffer)
+    dma_send.wait()
+    dma_recv.wait()
+    
+    print(output_buffer)
+    
+    prediction = output_buffer.copy()
+    print(prediction)
+    
+    dma_recv.transfer(output_buffer)
+    confidence = output_buffer.copy()
+    print(confidence)
+    
+    values = []
+
+    for i in range(10):
+        output_buffer.fill(0)
+        dma_recv.transfer(output_buffer)
+        dma_recv.wait()
+        values.append(output_buffer[0])  # Assuming each buffer has a single integer
+
+    print(values)  # This will print the list of integer values
+    
+    max_val_pred = generate_output(values)
+    print(max_val_pred)
+    
+    T = 1
+    predicted_prob = temperature_scaled_probability(values, T)
+    
+    print(predicted_prob)
+    
+    if predicted_prob < 0.94:
+        prediction = [9]
+    return prediction
+    
+    
+def send_data(connection, data):
+    connection.sendall(str(data).encode())  # Send data to the client
+
+    
+def receive_data(connection) -> bytes:
+        buffer = b""  # Holds the incoming data chunks
+        data_length = None  # Length of the full data
+
+        while True:
+            chunk = connection.recv(2048)
+            if not chunk:
+                # If recv returns an empty byte string, the connection is closed
+                raise ConnectionError("Socket connection closed prematurely")
+
+            buffer += chunk
+
+            # If data length is not determined, check if we have received the full length prefix
+            if data_length is None:
+                if b"_" in buffer:
+                    # split at first occurrence of _ to get the length of the payload
+                    length_str, remaining = buffer.split(b"_", 1)
+                    try:
+                        data_length = int(length_str.decode("utf-8"))
+                    except ValueError:
+                        raise ValueError("Invalid length prefix received")
+
+                    # The remaining part of the buffer is part of the actual data
+                    buffer = remaining
+
+            # If the total data received matches or exceeds the expected length, we are done
+            if data_length is not None and len(buffer) >= data_length:
+                # Extract the full data portion
+                full_data = buffer[:data_length]
+                return full_data
+        
+# Thread 3: Reads from the receive_queue, processes the data, and puts it in the send_queue
+def process_data(conn):
+    print("process_data starting")
+    while True:
+
+        print("receiving data")
+        data_string = receive_data(conn).decode()
+
+        #print(f"Complete Data for ML: {data_string}")
+        if data_string:
+            data = ast.literal_eval(data_string)
+            processed_data = MLmodel(data)
+
+            print(f"Processed data: {processed_data}")
+            send_data(conn,processed_data)
+
+def start_server():
+    #host = "localhost"
+    host = "0.0.0.0"
+#     host = "127.0.0.1"
+    port = 65433
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print("Server: Listening for connections...")
+        
+        while True:
+            try:
+                conn, addr = s.accept()
+                print(f"Server: Connected to {addr}")
+                process_data(conn)
+            except Exception as e:
+                conn.close()
+                print(f"exception: {e}")
+
+def main():
+    initialise()
+    start_server()
+    
+def handle_sigint(signum, frame):
+    print("Shutting down server")
+    sys.exit(0)
+    
+signal.signal(signal.SIGINT, handle_sigint)
+        
+if __name__ == "__main__":
+    main()  
